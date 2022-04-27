b5a8455cc6b23d23bc7b8bd0299ed58ac8033b35
Ananya Agrawal <ananya.199303010@muj.manipal.edu>
2022-04-27 11:43:47 +0530

    edited Q10 for fibonacci series

--- Q10.sh
+++ Q10.sh
@@ -3,4 +3,37 @@
 echo "Enter a value upto where the Fibonacci sequence has to be printed:"
 read n
 
+first=0
+second=1
+i=2
 
+echo "Fibonacci Sequence upto " $n " terms is:"
+if [ $n -eq 0 ]
+then
+	echo "NULL"
+	exit 0
+fi
+if [ $n -eq 1 ]
+then
+	echo "$first"
+	exit 0
+fi
+if [ $n -eq 2 ]
+then
+	echo "$first"
+	echo "$second"
+	exit 0
+fi
+if [ $n -ge 3 ]
+then
+	echo "$first"
+	echo "$second"
+while [ $i -lt $n ]
+do
+	next=$(( $first + $second ))
+	echo "$next"
+	first=$second
+	second=$next
+	i=$(( $i + 1 ))
+done
+fi
