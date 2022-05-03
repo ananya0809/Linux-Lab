688d5a518f02351c819542fdfc11105aae38a413
Ananya Agrawal <ananya.199303010@muj.manipal.edu>
2022-05-04 00:52:24 +0530

    edited

--- Q14.sh
+++ Q14.sh
@@ -3,7 +3,7 @@
 echo "Enter the name of a Directory under the present working directory"
 read directory
 
-num=$(find $directory -type f|wc -l)
+num=$(find $directory -type f|ls $directory|wc -l)
 #num=$(($num - 1 ))
 
 echo "The number of files in the given Directory are:" $num
